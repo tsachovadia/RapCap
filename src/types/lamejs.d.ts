ca2330e3d98ceae6258001cae08fdfd734862bdb
syrianHammer <tsach.ovadia@post.runi.ac.il>
2026-01-30 13:38:58 +0700

    fix: resolve lamejs MPEGMode error by using bundled lame.all.js

--- src/types/lamejs.d.ts
+++ src/types/lamejs.d.ts
@@ -4,8 +4,13 @@ declare module 'lamejs' {
         encodeBuffer(left: Int16Array, right?: Int16Array): Int8Array;
         flush(): Int8Array;
     }
+}
+
+declare module 'lamejs/lame.all.js' {
+    import * as lame from 'lamejs';
+    export const Mp3Encoder: typeof lame.Mp3Encoder;
     const lamejs: {
-        Mp3Encoder: typeof Mp3Encoder;
+        Mp3Encoder: typeof lame.Mp3Encoder;
     };
     export default lamejs;
 }
